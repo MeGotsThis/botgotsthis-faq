03ef46b425e20940a1a680d764d8967b4ada5e10
MeGotsThis <megotsthis@gmail.com>
2017-10-10 01:13:03 -0700

    Separating the database calls to functions

--- channel.py
+++ channel.py
@@ -1,32 +1,18 @@
 ﻿from datetime import timedelta
-from typing import Any, Optional, Tuple  # noqa: F401
-
-import aioodbc.cursor  # noqa: F401
+from typing import Optional  # noqa: F401
 
 from lib.data import ChatCommandArgs
 from lib.helper.chat import cooldown, permission
+from . import library
 
 
 @cooldown(timedelta(seconds=15), 'faq', 'moderator')
 async def commandFaq(args: ChatCommandArgs) -> bool:
-    cursor: aioodbc.cursor.Cursor
-    async with await args.database.cursor() as cursor:
-        query: str
-        faqRow: Optional[Tuple[str]]
-        faq: Optional[str]
+    faq: Optional[str] = await library.getFaq(args.database, args.chat.channel)
 
-        query = 'SELECT faq FROM faq WHERE broadcaster=?'
-        await cursor.execute(query, (args.chat.channel,))
-        faqRow = await cursor.fetchone()
-        faq = (faqRow or [None])[0]
-
-        if faq is None:
-            query = '''
-SELECT faq FROM faq_game WHERE broadcaster=? AND twitchGame=?'''
-            params = args.chat.channel, args.chat.twitchGame
-            await cursor.execute(query, params)
-            faqRow = await cursor.fetchone()
-            faq = (faqRow or [None])[0]
+    if faq is None:
+        faq = await library.getGameFaq(args.database, args.chat.channel,
+                                       args.chat.twitchGame)
 
     if faq:
         args.chat.send(f'FAQ: {faq}')
@@ -38,65 +24,35 @@ No FAQ was set. Use !setfaq or !setgamefaq to set a FAQ''')
 
 @permission('moderator')
 async def commandSetFaq(args: ChatCommandArgs) -> bool:
-    cursor: aioodbc.cursor.Cursor
-    async with await args.database.cursor() as cursor:
-        query: str
-        params: Tuple[Any, ...]
+    result: bool = await library.setFaq(args.database, args.chat.channel,
+                                        args.message.query)
+    if result:
         if not args.message.query:
-            query = 'DELETE FROM faq WHERE broadcaster=?'
-            await cursor.execute(query, (args.chat.channel,))
             args.chat.send('FAQ is now unset')
         else:
-            if args.database.isSqlite:
-                query = 'REPLACE INTO faq (broadcaster, faq) VALUES (?, ?)'
-                params = args.chat.channel, args.message.query
-            else:
-                query = '''\
-INSERT INTO faq (broadcaster, faq) VALUES (?, ?)
-    ON CONFLICT ON CONSTRAINT faq_pkey
-    DO UPDATE SET faq=?
-'''
-                params = (args.chat.channel, args.message.query,
-                          args.message.query)
-            await cursor.execute(query, params)
             args.chat.send(f'FAQ set as {args.message.query}')
-        await args.database.commit()
+    else:
+        args.chat.send(f'Error setting FAQ')
     return True
 
 
 @permission('moderator')
 async def commandSetGameFaq(args: ChatCommandArgs) -> bool:
     if not args.chat.twitchGame:
-        args.chat.send(args.chat.channel + ' needs to have a game set on '
-                       'Twitch before this command can be used')
+        args.chat.send(f'''\
+{args.chat.channel} needs to have a game set on Twitch before this command \
+can be used''')
 
-    cursor: aioodbc.cursor.Cursor
-    async with await args.database.cursor() as cursor:
-        query: str
-        params: Tuple[Any, ...]
+    result: bool = await library.setFaq(args.database, args.chat.channel,
+                                        args.message.query)
+    if result:
         if not args.message.query:
-            query = 'DELETE FROM faq_game WHERE broadcaster=? AND twitchGame=?'
-            params = args.chat.channel, args.chat.twitchGame
-            await cursor.execute(query, params)
             args.chat.send(f'''\
 FAQ is now unset for the game "{args.chat.twitchGame}"''')
         else:
-            if args.database.isSqlite:
-                query = '''
-REPLACE INTO faq_game (broadcaster, twitchGame, faq) VALUES (?, ?, ?)
-'''
-                params = (args.chat.channel, args.chat.twitchGame,
-                          args.message.query)
-            else:
-                query = '''\
-INSERT INTO faq_game (broadcaster, twitchGame, faq) VALUES (?, ?, ?)
-    ON CONFLICT ON CONSTRAINT faq_game_pkey
-    DO UPDATE SET faq=?
-'''
-                params = (args.chat.channel, args.chat.twitchGame,
-                          args.message.query, args.message.query)
-            await cursor.execute(query, params)
             args.chat.send(f'''\
 FAQ set as {args.message.query} for the game "{args.chat.twitchGame}"''')
-        await args.database.commit()
+    else:
+        args.chat.send(f'''\
+Error setting FAQ for game "{args.chat.twitchGame}"''')
     return True
---
+++ library.py
@@ -0,0 +1,84 @@
+﻿from typing import Any, Optional, Tuple  # noqa: F401
+
+import aioodbc.cursor  # noqa: F401
+
+from lib.database import DatabaseMain
+
+
+async def getFaq(database: DatabaseMain,
+                 channel: str) -> Optional[str]:
+    cursor: aioodbc.cursor.Cursor
+    async with await database.cursor() as cursor:
+        query: str = 'SELECT faq FROM faq WHERE broadcaster=?'
+        await cursor.execute(query, (channel,))
+        return (await cursor.fetchone() or [None])[0]
+
+
+async def getGameFaq(database: DatabaseMain,
+                     channel: str,
+                     game: str) -> Optional[str]:
+    cursor: aioodbc.cursor.Cursor
+    async with await database.cursor() as cursor:
+        query: str = '''
+SELECT faq FROM faq_game WHERE broadcaster=? AND twitchGame=?'''
+        await cursor.execute(query, (channel, game))
+        return (await cursor.fetchone() or [None])[0]
+
+
+async def setFaq(database: DatabaseMain,
+                 channel: str,
+                 faq: str) -> bool:
+    cursor: aioodbc.cursor.Cursor
+    async with await database.cursor() as cursor:
+        query: str
+        params: Tuple[Any, ...]
+        if not faq:
+            query = 'DELETE FROM faq WHERE broadcaster=?'
+            await cursor.execute(query, (channel,))
+        else:
+            if database.isSqlite:
+                query = 'REPLACE INTO faq (broadcaster, faq) VALUES (?, ?)'
+                params = channel, faq
+            else:
+                query = '''\
+INSERT INTO faq (broadcaster, faq) VALUES (?, ?)
+    ON CONFLICT ON CONSTRAINT faq_pkey
+    DO UPDATE SET faq=?
+'''
+                params = channel, faq, faq
+            await cursor.execute(query, params)
+        await database.commit()
+    return True
+
+
+async def setGameFaq(database: DatabaseMain,
+                     channel: str,
+                     game: str,
+                     faq: str) -> Optional[bool]:
+    if not game:
+        return None
+
+    cursor: aioodbc.cursor.Cursor
+    async with await database.cursor() as cursor:
+        query: str
+        params: Tuple[Any, ...]
+        if not faq:
+            query = 'DELETE FROM faq_game WHERE broadcaster=? AND twitchGame=?'
+            params = channel, game
+            await cursor.execute(query, params)
+        else:
+            if database.isSqlite:
+                query = '''
+REPLACE INTO faq_game (broadcaster, twitchGame, faq) VALUES (?, ?, ?)
+'''
+                params = channel, game, faq
+            else:
+                query = '''\
+INSERT INTO faq_game (broadcaster, twitchGame, faq) VALUES (?, ?, ?)
+    ON CONFLICT ON CONSTRAINT faq_game_pkey
+    DO UPDATE SET faq=?
+'''
+                params = channel, game, faq, faq
+            await cursor.execute(query, params)
+        await database.commit()
+    return True
