278cd5fca8c31806ab79498801ef93979bcf6eab
MeGotsThis <megotsthis@gmail.com>
2017-10-11 01:04:53 -0700

    Added tests for !setfaq

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,5 +1,6 @@
 from asynctest.mock import patch
 
+from lib.data.message import Message
 from tests.unittest.base_channel import TestChannel
 from tests.unittest.mock_class import StrContains
 
@@ -72,3 +73,62 @@ class TestChannelFaq(TestChannel):
         mock_faq.assert_called_once_with(self.database, self.channel.channel)
         self.assertFalse(mock_gamefaq.called)
         self.channel.send.assert_called_once_with(StrContains('FAQ', 'Kappa'))
+
+    @patch(library.__name__ + '.setGameFaq')
+    @patch(library.__name__ + '.setFaq')
+    async def test_setfaq_not_moderator(self, mock_faq, mock_gamefaq):
+        self.permissionSet['moderator'] = False
+        self.channel.twitchGame = 'Kappa'
+        mock_faq.return_value = False
+        mock_gamefaq.return_value = False
+        message = Message('!setfaq Kappa')
+        args = self.args._replace(message=message)
+        self.assertIs(await channel.commandSetFaq(args), False)
+        self.assertFalse(mock_faq.called)
+        self.assertFalse(mock_gamefaq.called)
+        self.assertFalse(self.channel.send.called)
+
+    @patch(library.__name__ + '.setGameFaq')
+    @patch(library.__name__ + '.setFaq')
+    async def test_setfaq(self, mock_faq, mock_gamefaq):
+        self.permissionSet['moderator'] = True
+        self.channel.twitchGame = 'Kappa'
+        mock_faq.return_value = True
+        mock_gamefaq.return_value = False
+        message = Message('!setfaq Kappa')
+        args = self.args._replace(message=message)
+        self.assertIs(await channel.commandSetFaq(args), True)
+        mock_faq.assert_called_once_with(
+            self.database, self.channel.channel, 'Kappa')
+        self.assertFalse(mock_gamefaq.called)
+        self.channel.send.assert_called_once_with(StrContains('FAQ', 'Kappa'))
+
+    @patch(library.__name__ + '.setGameFaq')
+    @patch(library.__name__ + '.setFaq')
+    async def test_setfaq_empty(self, mock_faq, mock_gamefaq):
+        self.permissionSet['moderator'] = True
+        self.channel.twitchGame = 'Kappa'
+        mock_faq.return_value = True
+        mock_gamefaq.return_value = False
+        message = Message('!setfaq')
+        args = self.args._replace(message=message)
+        self.assertIs(await channel.commandSetFaq(args), True)
+        mock_faq.assert_called_once_with(
+            self.database, self.channel.channel, '')
+        self.assertFalse(mock_gamefaq.called)
+        self.channel.send.assert_called_once_with(StrContains('FAQ', 'unset'))
+
+    @patch(library.__name__ + '.setGameFaq')
+    @patch(library.__name__ + '.setFaq')
+    async def test_setfaq_error(self, mock_faq, mock_gamefaq):
+        self.permissionSet['moderator'] = True
+        self.channel.twitchGame = 'Kappa'
+        mock_faq.return_value = False
+        mock_gamefaq.return_value = False
+        message = Message('!setfaq')
+        args = self.args._replace(message=message)
+        self.assertIs(await channel.commandSetFaq(args), True)
+        mock_faq.assert_called_once_with(
+            self.database, self.channel.channel, '')
+        self.assertFalse(mock_gamefaq.called)
+        self.channel.send.assert_called_once_with(StrContains('Error', 'FAQ'))
